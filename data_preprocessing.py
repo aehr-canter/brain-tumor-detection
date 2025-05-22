426dab9ee8a9f663a41bef2a2a2851703ece80ad
Aehr (Rhea) <mail2.rheaagr@gmail.com>
2025-05-23 02:16:17 +0530

    making first file

---
+++ data_preprocessing.py
@@ -0,0 +1,7 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
