fe4a58e8ec7ebc6f333889e0b50b6423377bcc8b
Aehr (Rhea) <mail2.rheaagr@gmail.com>
2025-05-23 11:41:18 +0530

    setup seed and directory

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,6 +2,17 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import tensorflow as tf
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+import random
+seed = 42
+random.seed(seed)
+np.random.seed(seed)
+tf.random.set_seed(seed)
+
+main_directory = 
+yes_directory = os.path.join(main_directory, 'yes')
+no_directory = os.path.join(main_directory, 'no')
